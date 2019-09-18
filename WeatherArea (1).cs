ab9dce261e769aa1a12f20605b0ee8b996074777
alliyah salonga <alliyahmaysalonga12@gmail.com>
2019-09-18 00:36:58 -0700

    weatherpanel Alliyah

---
+++ WeatherArea (1).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlliyahMaySalonga.WeatherPanel.windows
+{
+    public class WeatherArea
+    {
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+
+        public CurrentWeather Currently { get; set; }
+    }
+
+    public class CurrentWeather
+    {
+        public string Summary { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Temperature { get; set; }
+
+        public string Humidity { get; set; }
+
+        public string Pressure { get; set; }
+
+        public string Windspeed { get; set; }
+    }
+}
